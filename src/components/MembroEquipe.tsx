edc9c46b9e212aab1c117990a92c725594aa342e
elias.sales <elias.sales@fabricadenegocio.com.br>
2025-05-20 18:17:07 -0300

    site finalizado

--- src/components/MembroEquipe.tsx
+++ src/components/MembroEquipe.tsx
@@ -2,14 +2,15 @@ type MembroEquipeProps = {
     nome: string;
     cargo: string;
     imagem: string;
+    className?: string;
 }
 
-export const MembroEquipe =({ nome, cargo, imagem }: MembroEquipeProps) => {
+export const MembroEquipe = ({ nome, cargo, imagem, className }: MembroEquipeProps) => {
     return (
-        <div className="bg-[#262626] text-white rounded-2xl p-4 border-2 border-[#017108] w-48 text-center">
+        <div className={`membro-card ${className || ''}`}>
             <img src={imagem} alt={`Foto de ${nome}.`} className="w-32 h-32 object-cover rounded-xl mx-auto mb-2" />
             <h3 className="font-semibold">{nome}</h3>
-            <p className="text-sm">{cargo}</p>
+            <p className="text-sm whitespace-pre-line">{cargo}</p>
         </div>
     );
 };
