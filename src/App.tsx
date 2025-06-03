b2f198d478d917e05014cc8df5fef5b07385cf43
elias.sales <elias.sales@fabricadenegocio.com.br>
2025-06-02 22:07:13 -0300

    novo design

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,36 @@
 import React, { useState } from 'react';
 import './App.css';
-import headerPic from '/assets/headerPic.png';
+import headerPic from '/assets/foto-de-fundo.jpg';
 import logo from '/assets/logo.png';
+import parede from '/assets/escrita-parede.png';
+import wpp from '/assets/whatsapp.svg';
+import raizes from '/assets/raízes.jpg';
+import telhado from '/assets/telhado.jpg';
 import { MembroEquipe } from './components/MembroEquipe';
-import Tree from '/assets/Tree.png';
-
-import blindGamingLogo from '/assets/logo.png';
 
 function App() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-  const handleEmailSend = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const form = event.currentTarget;
-    const descriptionTextarea = form.elements.namedItem('description') as HTMLTextAreaElement;
-
-    const userDescription = descriptionTextarea ? descriptionTextarea.value : '';
-
-    const recipientEmail = 'ricardoj.lima@unifacol.edu.br';
-    const subject = 'Contato via Blind Gaming Website';
-    const bodyEmail = `Descrição:\n${userDescription}`;
-
-    window.open(
-      `mailto:${recipientEmail}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(bodyEmail)}`,
-      '_blank'
-    );
-  };
-
-  const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
-  };
-
-  const closeMobileMenu = () => {
-    setIsMobileMenuOpen(false);
-  };
+  const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
+  const closeMobileMenu = () => setIsMobileMenuOpen(false);
 
   return (
-    <div className="w-full bg-[#222222] text-white">
-      <header className="fixed top-0 left-0 w-full z-50 bg-black">
+    <div className="w-full bg-white text-white">
+      <header className="fixed top-0 left-0 w-full z-50 bg-white text-black">
         <div className="relative w-full h-[80px] md:h-[105px]">
           <div className="absolute inset-0 flex items-center justify-between max-w-6xl mx-auto px-4 md:px-8">
             <a href="#" onClick={closeMobileMenu}>
               <img className="h-[50px] md:h-[70px]" src={logo} alt="Logo menor" />
             </a>
             <nav className="hidden md:flex gap-4 md:gap-6 font-semibold">
-              <a href="#blind-gaming">QUEM SOMOS</a>
-              <a href="#nosso-produto">NOSSO PRODUTO</a>
+              <a href="#blind-gaming">BLIND GAMING</a>
+              <a href="#nosso-produto">RAÍZES DO MEDO</a>
               <a href="#equipe">EQUIPE</a>
               <a href="#contato">CONTATO</a>
             </nav>
             <button
-              className="md:hidden text-white text-2xl focus:outline-none"
+              className="md:hidden text-black text-2xl"
               onClick={toggleMobileMenu}
-              aria-label="Toggle mobile menu"
             >
               &#9776;
             </button>
@@ -61,17 +38,17 @@ function App() {
         </div>
 
         {isMobileMenuOpen && (
-          <div className="md:hidden fixed top-[80px] left-0 w-full bg-black bg-opacity-95 flex flex-col items-center py-4 z-40">
-            <a href="#blind-gaming" className="py-2 text-lg font-semibold" onClick={closeMobileMenu}>
-              QUEM SOMOS
+          <div className="md:hidden fixed top-[80px] left-0 w-full bg-white text-black bg-opacity-95 flex flex-col items-center py-4 z-40">
+            <a href="#blind-gaming" onClick={closeMobileMenu} className="py-2 text-lg font-semibold">
+              BLIND GAMING
             </a>
-            <a href="#nosso-produto" className="py-2 text-lg font-semibold" onClick={closeMobileMenu}>
-              NOSSO PRODUTO
+            <a href="#nosso-produto" onClick={closeMobileMenu} className="py-2 text-lg font-semibold">
+              RAÍZES DO MEDO
             </a>
-            <a href="#equipe" className="py-2 text-lg font-semibold" onClick={closeMobileMenu}>
+            <a href="#equipe" onClick={closeMobileMenu} className="py-2 text-lg font-semibold">
               EQUIPE
             </a>
-            <a href="#contato" className="py-2 text-lg font-semibold" onClick={closeMobileMenu}>
+            <a href="#contato" onClick={closeMobileMenu} className="py-2 text-lg font-semibold">
               CONTATO
             </a>
           </div>
@@ -79,125 +56,117 @@ function App() {
       </header>
 
       <div className="pt-[80px] md:pt-[105px]">
-        <div className="relative w-full h-[100vh] md:h-[100vh]">
+        {/* Hero */}
+        <div className="relative w-full h-[100vh]">
           <img className="w-full h-full object-cover" src={headerPic} alt="Header" />
           <div className="absolute top-0 left-0 w-full h-full flex items-center px-4 md:px-8">
-            <div className="flex flex-col md:flex-row items-center md:justify-between w-full max-w-5xl mx-auto">
-              <img src={logo} alt="Logo grande" className="w-[100px] md:w-[170px] mt-2 md:mt-2" />
-
-              <div className="flex flex-col items-center md:items-end mt-4 md:mt-0">
-                <h1 className="text-white text-base md:text-2xl font-bold text-center md:text-right leading-snug">
-                  DESENVOLVENDO JOGOS PARA<br />PESSOAS COM DEFICIÊNCIA VISUAL
-                </h1>
-                <a
-                  href="#blind-gaming"
-                  className="mt-4 bg-black bg-opacity-70 text-white font-semibold py-2 px-6 text-sm rounded-lg transition duration-300 ease-in-out hover:bg-opacity-90 hover:cursor-pointer md:mr-[90px]"
-                  onClick={closeMobileMenu}
-                >
-                  Conheça
-                </a>
-              </div>
+            <div className="flex flex-col items-center w-full">
+              <h1 className="text-white text-base md:text-2xl font-bold text-center leading-snug">
+                DESENVOLVENDO JOGOS PARA<br />PESSOAS COM DEFICIÊNCIA VISUAL
+              </h1>
+              <a
+                href="#blind-gaming"
+                className="mt-4 bg-black bg-opacity-70 text-white font-semibold py-2 px-6 text-sm rounded-lg hover:bg-opacity-90"
+              >
+                Conheça
+              </a>
             </div>
           </div>
         </div>
 
-        <div>
-          <div className="purple-gradient pt-6 pb-11 shadow-lg">
-            <h2
-              id="blind-gaming"
-              className="text-xl md:text-2xl font-semibold text-center m-4 scroll-mt-[90px] md:scroll-mt-[110px]"
-            >
-              Quem Somos
-            </h2>
-            <p className="text-center px-4 md:px-80 text-justify text-sm md:text-xl font-semibold">
-              A Blind Gaming é uma empresa brasileira de tecnologia e desenvolvimento de jogos digitais, fundada com o
-              propósito de tornar o mundo dos games mais acessível e inclusivo. Seu foco principal está em criar
-              experiências imersivas que possam ser aproveitadas por pessoas com cegueira, sem abrir mão da qualidade
-              técnica e artística que encanta todos os públicos. A Blind Gaming trabalha para garantir que acessibilidade
-              e diversão caminhem lado a lado.
-            </p>
-          </div>
-
-          <section className="relative bg-[#222222] text-white py-5">
-            <div className="relative z-10 max-w-3xl mx-auto text-center px-4">
-              <div className="flex justify-center -mb-10 md:-mb-18 -mt-8 md:-mt-12">
-                <img src={Tree} alt="Árvore" className="w-[60%] md:w-[40%]" />
-              </div>
-              <h2
-                id="nosso-produto"
-                className="text-xl md:text-2xl font-semibold mb-4 scroll-mt-[90px] md:scroll-mt-[110px]"
-              >
-                Nosso Produto
-              </h2>
-              <p className="text-sm md:text-lg text-justify font-semibold mb-7">
-                Raízes do Medo é um jogo que busca se destacar por sua acessibilidade, sendo projetado especialmente para incluir pessoas com deficiência visual. Utilizando sons e comandos como base de interatividade, oferece uma experiência inclusiva. Além disso, foca no terror psicológico, com design de áudio avançado que cria uma ambientação intensa, proporcionando uma jogabilidade imersiva e única para todos os públicos.
+        {/* Sobre */}
+        <section className="bg-white text-black pt-6 pb-11 shadow-lg">
+          <h2 id="blind-gaming" className="text-xl md:text-2xl font-semibold text-center m-4">
+            Blind Gaming
+          </h2>
+          <div className='flex flex-col md:flex-row items-center justify-center max-w-6xl mx-auto px-4 md:px-8'>
+            <div className="w-full md:w-1/2 text-justify">
+              <p className="text-sm md:text-xl font-semibold">
+                A Blind Gaming é uma empresa brasileira de desenvolvimento de jogos digitais, fundada com o propósito de proporciar experiências inclusivas e acessíveis. <br /><br />
+                Nossa missão é criar experiências que possam ser aproveitadas por pessoas com deficiência visual, sem abrir mão da qualidade técnica e artística. <br /><br />
+                Nosso trabalho é garantir que acessibilidade e diversão caminhem lado a lado.
               </p>
             </div>
-          </section>
-
-          <div className="equipe-container text-center space-y-6 bg-transparent">
-            <h2
-              id="equipe"
-              className="text-xl md:text-2xl font-semibold scroll-mt-[90px] md:scroll-mt-[110px]"
-            >
-              Equipe
-            </h2>
-            <div className="equipe-cards flex flex-wrap justify-center gap-4 md:gap-6 px-4">
-              <MembroEquipe className="membro-card" nome="Ricardo Junior" cargo="Technical Writer" imagem="./assets/Ricardo.jpg" />
-              <MembroEquipe className="membro-card" nome="Rhuan Victor" cargo={'Product Owner\nAgilista\nSoftware Engineer'} imagem="./assets/Rhuan.jpg" />
-              <MembroEquipe className="membro-card" nome="Elias Sales" cargo="Software Engineer" imagem="./assets/Elias.jpg" />
-              <MembroEquipe className="membro-card" nome="Luan Santos" cargo="Quality Assurance" imagem="./assets/Luan.jpg" />
-              <MembroEquipe className="membro-card" nome="Lucas Oliveira" cargo={'Designer\nTechnical Writer'} imagem="./assets/Lucas.jpg" />
+            <div className="w-full md:w-1/2 flex justify-center items-center mt-8 md:mt-0">
+              <img src={logo} alt="Logo" className='h-[20vh] md:h-[30vh] object-contain' />
             </div>
           </div>
+        </section>
 
-          <div className="contact-section bg-[#222222] py-10 px-4">
-            <h2
-              id="contato"
-              className="text-xl md:text-2xl font-semibold mb-8 text-center scroll-mt-[90px] md:scroll-mt-[110px]"
-            >
-              Entre em contato com a gente!
+        {/* Produto */}
+        <section className="bg-black text-white py-5">
+          <div className="max-w-3xl mx-auto text-center px-4">
+            <div className="hidden md:flex justify-center gap-[150px] mb-8">
+              <img src={parede} alt="parede" className="w-[250px]" />
+              <img src={raizes} alt="raízes" className="w-[250px]" />
+              <img src={telhado} alt="telhado" className="w-[250px]" />
+            </div>
+            <div className="flex md:hidden justify-center mb-8">
+              <img src={parede} alt="Imagem Mobile" className="w-[60%]" />
+            </div>
+            <h2 id="nosso-produto" className="text-xl md:text-2xl font-semibold mb-4">
+              Raízes do Medo
             </h2>
+            <p className="text-sm md:text-lg text-justify font-semibold">
+              Criado com foco na acessibilidade, Raízes do Medo é um jogo de terror que garante uma imersão completa para jogadores com limitações visuais, sem comprometer o terror da narrativa.
+            </p>
+          </div>
+        </section>
 
-            <div className="contact-content flex flex-col md:flex-row justify-center items-center gap-8 md:gap-10 max-w-4xl mx-auto">
-              <form onSubmit={handleEmailSend} className="flex flex-col gap-4 w-full md:w-1/2">
-                <div className="flex flex-col">
-                  <textarea
-                    id="description"
-                    name="description"
-                    rows={5}
-                    placeholder="Descreva sua mensagem aqui..."
-                    className="bg-[#333333] border border-gray-600 rounded-md p-2 text-white focus:outline-none focus:border-[#6A019C] text-sm md:text-base"
-                    required
-                  ></textarea>
-                </div>
-                <button
-                  type="submit"
-                  className="bg-[#6A019C] hover:bg-[#7C00B6] text-white font-bold py-2 px-4 rounded-md transition duration-300 ease-in-out text-base md:text-lg hover:cursor-pointer"
+        {/* Equipe */}
+        <div className='flex gap-8 justify-center items-start'>          
+          <section className="equipe-container text-center py-10 md:py-16">
+            <h2 id="equipe" className="text-xl md:text-2xl font-semibold mb-8 md:mb-12">
+              Nossa Equipe
+            </h2>
+            <div className="flex flex-col md:flex-row justify-center items-center gap-6 px-4 max-w-6xl mx-auto">
+              <MembroEquipe nome="Ricardo Junior" cargo="Technical Writer" imagem="/assets/Ricardo.jpg" />
+              <MembroEquipe nome="Rhuan Victor" cargo={'Product Owner\nAgilista\nSoftware Engineer'} imagem="/assets/Rhuan.jpg" />
+              <MembroEquipe nome="Luan Santos" cargo="Quality Assurance" imagem="/assets/Luan.jpg" />
+              <MembroEquipe nome="Lucas Oliveira" cargo={'Designer\nTechnical Writer'} imagem="/assets/Lucas.jpg" />
+              <MembroEquipe nome="Elias Sales" cargo="Software Engineer" imagem="/assets/Elias.jpg" />
+            </div>
+          </section>
+        </div>
+
+        {/* Contato */}
+        <section className="bg-white text-black py-10 px-4">
+          <div className="max-w-4xl mx-auto">
+            <h2 id="contato" className="text-xl md:text-2xl font-semibold mb-4 text-center">
+              Restou alguma dúvida?
+            </h2>
+            <div className="flex flex-col md:flex-row justify-between items-center max-w-2xl mx-auto">
+              <div className="text-center md:text-left md:w-1/2 mb-8 md:mb-0">
+                <p className='font-semibold mb-8'>
+                  Entre em contato com a nossa equipe para tirar as suas dúvidas:
+                </p>
+                <a
+                  href="https://wa.me/5581987031072?text=Olá,%20tenho%20uma%20dúvida%20sobre%20a%20Blind%20Gaming."
+                  target='_blank' rel="noopener noreferrer"
+                  className="flex items-center justify-center md:justify-start"
                 >
-                  Enviar e-mail
-                </button>
-              </form>
-
-              <div className="flex flex-col items-center justify-center w-full md:w-1/2 mt-8 md:mt-0">
-                <img
-                  src={blindGamingLogo}
-                  alt="Logo Blind Gaming"
-                  className="w-[120px] md:w-[250px] h-auto mb-4"
-                />
+                  <button className="bg-[#019C3C] hover:bg-[#017e30] text-black font-semibold py-3 px-6 rounded-md text-lg flex items-center hover:cursor-pointer">
+                    <img src={wpp} alt="WhatsApp" className='w-[30px] mr-[20px]' />
+                    ENTRAR EM CONTATO
+                  </button>
+                </a>
+              </div>
+              <div className="flex justify-center md:justify-end md:w-1/2">
+                <img src={logo} alt="Logo Blind Gaming" className="w-[150px] md:w-[250px] h-auto" />
               </div>
             </div>
           </div>
+        </section>
 
-          <footer className="bg-[#222222] text-white px-4 md:px-8 py-4 md:py-6 border-t border-gray-700 text-center md:text-left">
-            <div className="flex flex-col sm:flex-row justify-between items-center max-w-7xl mx-auto">
-              <span className="font-semibold text-xs md:text-sm mb-2 sm:mb-0">
-                UNIFACOL - Centro Universitário FACOL, 2025.
-              </span>
-              <span className="text-xs">© BLIND GAMING - TODOS OS DIREITOS RESERVADOS.</span>
-            </div>
-          </footer>
-        </div>
+        {/* Rodapé */}
+        <footer className="bg-white text-black px-4 md:px-8 py-4 md:py-6 text-center">
+          <div className="flex flex-col sm:flex-row justify-between items-center max-w-7xl mx-auto">
+            <span className="font-semibold text-xs md:text-sm mb-2 sm:mb-0">
+              UNIFACOL - Centro Universitário FACOL, 2025.
+            </span>
+            <span className="text-xs">© BLIND GAMING - TODOS OS DIREITOS RESERVADOS.</span>
+          </div>
+        </footer>
       </div>
     </div>
   );
--- src/components/MembroEquipe.tsx
+++ src/components/MembroEquipe.tsx
@@ -1,16 +1,21 @@
 type MembroEquipeProps = {
-    nome: string;
-    cargo: string;
-    imagem: string;
-    className?: string;
-}
+  nome: string;
+  cargo: string;
+  imagem: string;
+};
 
-export const MembroEquipe = ({ nome, cargo, imagem, className }: MembroEquipeProps) => {
-    return (
-        <div className={`membro-card ${className || ''}`}>
-            <img src={imagem} alt={`Foto de ${nome}.`} className="w-32 h-32 object-cover rounded-xl mx-auto mb-2" />
-            <h3 className="font-semibold">{nome}</h3>
-            <p className="text-sm whitespace-pre-line">{cargo}</p>
-        </div>
-    );
+export const MembroEquipe = ({ nome, cargo, imagem }: MembroEquipeProps) => {
+  return (
+    <div className="flex flex-col justify-start items-center p-4 w-[180px]">
+      <div className="border-4 border-black rounded-xl overflow-hidden">
+        <img
+          src={imagem}
+          alt={`Foto de ${nome}`}
+          className="w-[150px] h-[180px] object-cover"
+        />
+      </div>
+      <h3 className="font-semibold text-sm text-center mt-2">{nome}</h3>
+      <p className="text-xs whitespace-pre-line text-center">{cargo}</p>
+    </div>
+  );
 };
